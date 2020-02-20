eb71c4c3e59809e72948420d48b9967e6dfeda8b
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-20 17:21:45 +0100

    modif memcpy

--- src/memcpy.s
+++ src/memcpy.s
@@ -8,9 +8,6 @@ memcpy:
     mov     rax, rdi                ; put the 1 st argument into return value
     cmp     rdx, 0                  ; compare if the 3 rd argument is egal to zero
     je      .EXIT                   ; go to exit
-    cmp     rsi, 0                  ; compare if the 2 nd arguement is egal to 0
-    je      .EXIT                   ; go to exit
-    dec     rdx                     ; decrement rdx to use like a index
 
 .LOOP:
     cmp     rdx, 0                  ; compare if rcx is egal to the 3 rd argument
@@ -18,7 +15,7 @@ memcpy:
     mov     al, BYTE [rsi]          ; put the value of the 2 nd arguement into al
     mov     BYTE [rdi], al          ; put the value of the 2 nd argument into the 1st arguement
     dec     rdx                     ; deincrementation of rdx
-    inc     rdi                     ; incrementation of rdi
+    inc     rdi                     ; incrementation
     jmp     .LOOP                   ; go to the loop
 
 .EXIT:
