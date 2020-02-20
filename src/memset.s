0b0f65e9b67a826e6f308a2a50459791db4aaed3
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-20 17:09:28 +0100

    correction memset

--- src/memset.s
+++ src/memset.s
@@ -6,14 +6,16 @@ BITS 64
 
 memset:
     mov     rax, rdi        ; move the 1 fst argument to the return value
+    cmp     rdx, 0          ; check if the arguement is egal to zero
+    jz      .EXIT           ; go to exit
     mov     rcx, rdx        ; move the 3 rd  argument to the 4 th argument (uninitialisate)
 
 .LOOP:
-    jrcxz   .EXIT           ; go to exit if rcx is egal to zero
     cmp     rcx, 0          ; compare rcx (egal to the arguement 3 rd) is egal to zero
     je      .EXIT           ; go to exit
     mov     BYTE [rdi], sil ; put the second arguement into the byte postion of the pointer
     dec     rcx             ; decrement rcx
+    inc     rdi             ; increment rdi
     jmp     .LOOP           ; go to himself
 
 .EXIT:
