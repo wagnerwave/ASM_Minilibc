ea0037ab020392ef35711fe73f8b7885b882d040
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-20 14:40:14 +0100

    change memset

--- src/memset.s
+++ src/memset.s
@@ -5,16 +5,16 @@ BITS 64
 	section .text
 
 memset:
-    mov     rax, rdi    ; move the 1 fst argument to the return value
-    mov     rcx, rdx    ; move the 3 rd  argument to the 4 th argument (uninitialisate)
+    mov     rax, rdi        ; move the 1 fst argument to the return value
+    mov     rcx, rdx        ; move the 3 rd  argument to the 4 th argument (uninitialisate)
 
 .LOOP:
-    jrcxz   .EXIT       ; jump to the exit function if rdx is egal to 0
-    cmp     [rax], sil  ; compare the value of the sil is pointing into the rax register
-    dec     rcx         ; decrement rcx
-    inc     rax         ; increment rax
-    jmp     .LOOP       ; go to himself
+    jrcxz   .EXIT           ; go to exit if rcx is egal to zero
+    cmp     rcx, 0          ; compare rcx (egal to the arguement 3 rd) is egal to zero
+    je      .EXIT           ; go to exit
+    mov     BYTE [rdi], sil ; put the second arguement into the byte postion of the pointer
+    dec     rcx             ; decrement rcx
+    jmp     .LOOP           ; go to himself
 
 .EXIT:
-    mov     rax, rdi    ; load the value of the rdi register into the rax register
-    ret                 ; return value
+    ret                 ; return value of rax
