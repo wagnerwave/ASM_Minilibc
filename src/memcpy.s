d6bb30d7e6f6deca62014c7ed9c04e6ad11d38ec
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-20 17:57:47 +0100

    correction memcpy

--- src/memcpy.s
+++ src/memcpy.s
@@ -6,16 +6,15 @@ BITS 64
 
 memcpy:
     mov     rax, rdi                ; put the 1 st argument into return value
-    cmp     rdx, 0                  ; compare if the 3 rd argument is egal to zero
-    je      .EXIT                   ; go to exit
 
 .LOOP:
     cmp     rdx, 0                  ; compare if rcx is egal to the 3 rd argument
-    jz      .EXIT                   ; if egal go to exit
-    mov     al, BYTE [rsi]          ; put the value of the 2 nd arguement into al
-    mov     BYTE [rdi], al          ; put the value of the 2 nd argument into the 1st arguement
+    je      .EXIT                   ; if egal go to exit
+    mov     r8b, BYTE [rsi]         ; put the value of the 2 nd arguement into al
+    mov     BYTE [rdi], r8b         ; put the value of the 2 nd argument into the 1st arguement
     dec     rdx                     ; deincrementation of rdx
-    inc     rdi                     ; incrementation
+    inc     rsi                     ; incrementation of pointer of argument 2
+    inc     rdi                     ; incrementation of pointer of argument 1
     jmp     .LOOP                   ; go to the loop
 
 .EXIT:
