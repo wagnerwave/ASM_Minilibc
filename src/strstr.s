77143867d30e3ca6355c0e75cfedd34a8e35c8a5
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-21 16:27:53 +0100

    correction strstr

--- src/strstr.s
+++ src/strstr.s
@@ -5,41 +5,35 @@ BITS 64
 	section .text
 
 strstr:
-    cmp     BYTE [rdi], 0           ; check if the 1 st argument is NULL
-    je      .EXIT_FAIL              ; return NULL
-    cmp     BYTE [rsi], 0           ; check if the 2 nd argument is NULL
-    je      .EXIT_FAIL              ; return NULL
     mov     rax, rdi                ; put the 1 st argument to rax
+    jmp     .LOOP                   ; go to loop
+
+.INC_RAX:
+    inc     rax                     ; incrementation  of rax
 
 .LOOP:
     cmp     BYTE [rax], 0           ; check if rax is not NULL
-    je      .NOT_FOUND              ; go to not found
-    xor     rcx, rcx                ; initilisate the index
+    je      .EXIT_FAIL              ; go to exit fail
+    mov     rcx, rax                ; move arguement 1 to rcx
+    mov     rdx, rsi                ; move arguement two into rdx
 
 .WHILE:
-    mov     r8b, BYTE [rax]         ; put the index of argument 1 into r8b
-    cmp     BYTE [rsi + rcx], r8b   ; comparement the index of argument 1 with argument 2
-    jnz     .FIND                   ; go to find
-    cmp     r8b, 0                  ; compare if it is the end of the argument 1
-    jz      .FIND                   ; go to exit fail
-    inc     rax                     ; increment of arguement 1
+    cmp     BYTE [rdx], 0           ; check if the 1 st argument is egal to zero
+    jz      .RET                    ; go to ret
+    cmp     BYTE [rcx], 0           ; check if argument 2 if not egal to zero
+    jz      .EXIT_FAIL              ; if egal go to exit fail
+    mov     r8b, BYTE [rdx]         ; put the index of argument 1 into r8b
+    cmp     BYTE [rcx], r8b         ; comparement the index of argument 1 with argument 2
+    jne     .INC_RAX                ; if not egal go to inc rax
+    inc     rdx                     ; increment of arguement 1
     inc     rcx                     ; incrementation of the index
     jmp     .WHILE                  ; go the the while
 
-.FIND:
-    cmp     BYTE [rsi + rcx], 0     ; check if index is at the end of 2 nd argument
-    jz      .EXIT_WIN               ; go to exit win
-    inc     rax                     ; return value of rax
-    jmp     .LOOP                   ;
-
-.NOT_FOUND:
-    xor     rax, rax                ;
-    ret                             ;
-
-.EXIT_WIN:
-    sub     rax, rcx                ;
-    ret                             ;
-
 .EXIT_FAIL:
-    mov     rax, 0                  ; rax egal NULL
+    cmp    BYTE [rsi], 0            ;
+    jz     .RET
+    xor    rax, rax                 ; rax egal NULL
     ret                             ; return value of rax
+
+.RET:
+    ret
