1d8cf9554185a447e3406a2a520921d340aae8c5
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-21 14:01:13 +0100

    modification de rindex

--- src/rindex.s
+++ src/rindex.s
@@ -5,21 +5,13 @@ BITS 64
 	section .text
 
 rindex:
-    xor     eax, eax        ; set return value to zero
+    xor     rax, rax        ; set return value to zero
 
 .WHILE:
     mov     cl, BYTE [rdi]  ; put the index of argument 1 into al
     cmp     cl, sil         ; comparement the index of argument 1 with argument 2
-    je      .FIND           ; go to find
-    cmp     cl, 0           ; compare if it is the end of the argument 1
-    jz      .EXIT_FAIL      ; go to exit fail
+    cmove   rax, rdi        ; go to find
     inc     rdi             ; increment of arguement 1
-    jmp     .WHILE          ; go the the while
-
-.FIND:
-    mov     rax, rdi        ; move pointer of argument 1 to rax
-    ret                     ; return value of rax
-
-.EXIT_FAIL:
-    mov     rax, 0          ; return value egal NULL
-    ret                     ; return value of rax
+    cmp     cl, 0           ; compare if it is the end of the argument 1
+    jne     .WHILE          ; go to exit fail
+    ret                     ; go the the while
