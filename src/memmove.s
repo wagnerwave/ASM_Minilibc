0708e28b06dd010023c5975a63b0ccf166cd3d1c
Alexandre Wagner <alexandre1.wagner@epitech.eu>
2020-02-24 11:18:07 +0100

    memmove functionnal

--- src/memmove.s
+++ src/memmove.s
@@ -5,22 +5,29 @@ BITS 64
 	section .text
 
 memmove:
-    mov      rcx, 0                 ; set rcx to 0
-    jmp      .LOOP                  ; jump to loop
+    cmp    rsi, rdi             ; compare the 2nd argument to 1 st argument
+    jl     .CPY_MEMORY          ; jump if lower
+    mov    rax, rdi             ; put the 1 st argument to rax
+    mov    rcx, rdx             ; put the 3 nd argument to rcx
+    mov    rdx, rsi             ; put the 2 nd argument to rdx
+    cld                         ; clear redirection flag
+    rep    movsb                ; repete action of flag
+    mov    rsi, rdx             ; put the rdx  to 2nd argument
+    mov    rdi, rax             ; put rax to 1 st argument
+    ret                         ; return value of rax
 
-.INC_RCX:
-    inc     rcx                     ; increment rcx
+.CPY_MEMORY:
+    mov    rax, rdi             ; put the 1 st argument to rax
 
-.LOOP:
-    cmp     BYTE [rdi + rcx], 0     ; check if is not the end of the 2nd argument
-    jz      .EXIT_FAIL              ; go to EXIT FAIL
-    cmp     BYTE [rdi + rcx], sil   ; compare the index of 1st argument into sil
-    jne     .INC_RCX                ; if not egal go increment rcx
-    mov     rax, [rdi + rcx]         ; move the pointer info the return value
-    jmp     .RET                    ; go to return
+.WHILE:
+    cmp     rdx, 0              ; compare rdx to zero
+    je      .EXIT               ; if egal go to exit
+    mov     al, BYTE [rsi]      ; move the 2nd byte argument to rax (8bit)
+    mov     BYTE [rdi], al      ; compare the byte of 1 st argument with rax (8bit)
+    dec     rdx                 ; decrementation of rdx
+    inc     rdi                 ; incrementation of rdi
+    inc     rsi                 ; incrementation of 2 nd argument
+    jmp     .WHILE              ; go to WHILe
 
-.EXIT_FAIL:
-    xor     rax, rax                  ; set rax to 0
-
-.RET:
-    ret                             ; return value of rax
+.EXIT:
+    ret                         ; return value of rax
